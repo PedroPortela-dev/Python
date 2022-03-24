142e2db667e32c2f94cdad8006f2e197d0782a52
PedroPortela-dev <pedrocaspop64@gmail.com>
2022-03-23 22:10:49 -0300

    Foda-se

---
+++ python/CursoEmVideo.py
@@ -0,0 +1,11 @@
+nome = input("Qual é o seu nome?    ")
+print("Olá " + nome + " ! Prazer em te conhecer!")
+
+dia = input("Dia = ")
+mes = input("Mes = ")
+ano = input("Ano = ")
+print(nome + " vc nasceu no dia ",dia," de "+mes+" de " ,ano, " . Correto?")
+
+num1 = int(input("Primeiro numero:  "))
+num2 = int(input("Segundo numero:   "))
+print("{} + {} = {}".format(num1, num2, num1+num2))
---
+++ python/Teste.py
@@ -0,0 +1,14 @@
+import math
+
+n = input("Digite um valor:    ")
+print(n.isspace())
+print(n.isnumeric())
+print(n.isalpha())
+print(n.isalnum())
+print(n.isupper())
+print(n.islower())
+print(n.istitle())
+
+math.comb
+
+print(math.comb(7,5))
